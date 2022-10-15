6a59872be441acf71d2510721c06df749413f097
rvdb7345 <rvdb7345@gmail.com>
2022-10-15 21:13:46 +0200

    Composition of teams finished

--- feature_engineer.py
+++ feature_engineer.py
@@ -1,4 +1,15 @@
+import numpy as np
 import pandas as pd
+from tqdm import tqdm
+
+formations_dict = {'4-3-1-2': ['GK', 'RB|RWB', 'LCB|CB', 'RCB|CB', 'LB|LWB', 'CDM|CM', 'CDM|CM', 'CDM|CM', 'CAM|CF', 'CF|ST', 'CF|ST'],
+                   '4-3-2-1': ['GK', 'RB|RWB', 'LCB|CB', 'RCB|CB', 'LB|LWB', 'CDM|CM', 'CDM|CM', 'CDM|CM', 'CAM|CF', 'CAM|CF', 'CF|ST'],
+                   '4-3-3': ['GK', 'RB|RWB', 'LCB|CB', 'RCB|CB', 'LB|LWB', 'CDM|CM', 'CDM|CM', 'CDM|CM', 'RW|RF|ST', 'CF|ST', 'LW|LF|ST'],
+                   '4-4-2': ['GK', 'RB|RWB', 'RCB|CB', 'LCB|CB', 'LB|LWB', 'RM|RW', 'CDM|CM', 'CDM|CM', 'LM|LW', 'CF|ST', 'CF|ST'],
+                   '4-5-1': ['GK', 'RB|RWB', 'RCB|CB', 'LCB|CB', 'LB|LWB', 'RM|RW', 'CDM|CM', 'CDM|CM', 'LM|LW', 'CF|ST', 'CF|ST'],
+                   '3-4-1-2': ['GK', 'RCB|CB', 'CB', 'LCB|CB', 'RM|RW', 'CDM|CM', 'CDM|CM', 'LM|LW', 'CAM|CF', 'CF|ST', 'CF|ST'],
+                   '3-4-3': ['GK', 'RCB|CB', 'CB', 'LCB|CB', 'RWB|RM', 'CDM|CM', 'CDM|CM', 'LWB|LM', 'RW|RF|ST', 'CF|ST', 'LW|LF|ST'],
+                   '3-5-2': ['GK', 'RCB|CB', 'CB', 'LCB|CB', 'RM|RWB|RB', 'CDM|CM', 'CDM|CM', 'CDM|CM', 'LM|LWB|LB', 'CF|ST', 'CF|ST']}
 
 def load_players_per_team(team, player_list, national):
     """Load all players that could potentially have played."""
@@ -10,31 +21,122 @@ def load_players_per_team(team, player_list, national):
 
     return team_player_list
 
-def generate_fictive_team_from_players(players):
-    """We don't know how the teams were composed at the time of playing, therefore, we make a fictive team out of the
-    potential players we have.
+def obtain_best_formation(players, measurement='overall'):
+    """Find the formation for which we have the best players."""
 
-    This function could help
-    https://www.kaggle.com/code/stefanoleone992/fifa-22-players-lineup-visualization-and-more
-    """
+    # loop over the formations and check what we have players for
+    formations_total_vals = {}
+    for formation in formations_dict:
+        copied_df = players.copy()
+        pos_list = formations_dict[formation]
+        total_vals = []
+        for pos in pos_list:
+            # get best record based on 'overall' or 'potential',
+            # then drop that record from copied df, so that it cannot be selected again
+            if not np.isnan(copied_df[copied_df['player_positions'].str.contains(pos)][measurement].max()):
+                total_vals.append(copied_df[copied_df['player_positions'].str.contains(pos)][measurement].max())
+                copied_df.drop(copied_df[copied_df['player_positions'].str.contains(pos)][measurement].idxmax(),
+                               inplace=True)
+        if len(total_vals) == 11:
+            formations_total_vals[formation] = sum(total_vals)
+        else:
+            # some formations might not find 11 available players -
+            # these ones need to be excluded from any possible calcuation
+            formations_total_vals[formation] = 0
 
-    return
+    # return none if no possible configuration could be found
+    if sum(formations_total_vals.values()) == 0:
+        return None
+    else:
+        # return best formation
+        best_formation = max(formations_total_vals, key=formations_total_vals.get)
+        return best_formation
 
 
+def get_best_lineup(lineup_df, formation='', measurement=''):
+    """Select the best players by our selected formation."""
+    copy_df = lineup_df.copy()
+
+    # if formation is not chosen, then the best one is calculated with a formula
+    if formation == '':
+        formation = obtain_best_formation(copy_df, measurement)
+    squad_lineup = formations_dict[formation]
+
+    # select the best player for the position
+    composed_squad = []
+    for pos in squad_lineup:
+        best_player_record = copy_df.loc[[copy_df[copy_df['player_positions'].str.contains(pos)][measurement].idxmax()]]
+        composed_squad.append(best_player_record)
+        copy_df.drop(copy_df[copy_df['player_positions'].str.contains(pos)][measurement].idxmax(), inplace=True)
+    return formation, composed_squad
 
 
+def compose_best_squad(team):
+    """Compose the best possible team based on the available players."""
+    all_potential_players_in_team = load_players_per_team(team, player_22_df, row['national_game'])
+
+    if len(all_potential_players_in_team) >= 11:
+        best_formation = obtain_best_formation(all_potential_players_in_team)
+
+        if best_formation:
+            composed_team = get_best_lineup(all_potential_players_in_team,
+                                            formation=best_formation, measurement='overall')
+
+            assert len(composed_team[1]) == 11, f'Composition {team} not correct, ' \
+                                                f'{len(composed_team[1])} players'
+
+            return composed_team
+        else:
+            return None
+    else:
+        return None
+
+def change_club_name_to_match_fifa(club):
+    parsed_name = club
+    if club == 'China':
+        parsed_name = 'China PR'
+    if club == 'DR Congo':
+        parsed_name = 'Congo DR'
+    if club == 'USA':
+        parsed_name = 'United States'
+    if club == 'Guinea-Bissau':
+        parsed_name = 'Guinea Bissau'
+    return parsed_name
 
 if __name__ == '__main__':
     fixture_overview_df = pd.read_csv('prepped_data_sources/prepped_fixture_overview.csv')
     player_22_df = pd.read_csv('fifa_player_data/players_22.csv')
 
-    for idx, row in fixture_overview_df.iterrows():
+    composed_teams = {}
+    uncomposed_teams = []
+    for idx, row in tqdm(fixture_overview_df.iterrows()):
         hometeam = row['HomeTeam']
         awayteam = row['AwayTeam']
 
-        all_potential_players_in_home_team = load_players_per_team(hometeam, player_22_df, row['national_game'])
-        all_potential_players_in_away_team = load_players_per_team(awayteam, player_22_df, row['national_game'])
+        # change names to match fifa club names
+        hometeam = change_club_name_to_match_fifa(hometeam)
+        awayteam = change_club_name_to_match_fifa(awayteam)
 
+        # obtain best home and away teams
+        if hometeam not in composed_teams.keys():
+            best_home_team = compose_best_squad(hometeam)
+            if best_home_team:
+                composed_teams[hometeam] = best_home_team[1]
+            else:
+                uncomposed_teams.append(hometeam)
+        else:
+            best_home_team = composed_teams[hometeam]
 
+        if awayteam not in composed_teams.keys():
+            best_away_team = compose_best_squad(awayteam)
+            if best_away_team:
+                composed_teams[awayteam] = best_away_team[1]
+            else:
+                uncomposed_teams.append(awayteam)
+        else:
+            best_away_team = composed_teams[awayteam]
+
+    print(f'Team found for {len(composed_teams) / len(fixture_overview_df["HomeTeam"].unique())*100}% of the teams')
+    print(f'Uncomposed teams: {set(uncomposed_teams)}')
 
 
